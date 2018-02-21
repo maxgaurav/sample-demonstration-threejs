e32e9cdc7f28da3f1fcaa5f126bfb005ac3724ef
Max Gaurav <max.gaurav@ithands.net>
2018-02-21 17:25:36 +0530

    add who knows what

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import * as $ from 'jquery/dist/jquery.slim';
-import {BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer} from "three";
+import {
+    GridHelper, Mesh, MeshBasicMaterial, PerspectiveCamera, PlaneGeometry, Scene, TextureLoader,
+    WebGLRenderer
+} from "three";
+
 
 let scene = new Scene();
 let camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -13,14 +17,49 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 $('body').append(renderer.domElement);
 
 
-let geometry = new BoxGeometry(1,1,1);
+let geometry = new PlaneGeometry(0.1, 1);
 let material = new MeshBasicMaterial({color: 0x00ff00});
-let cube = new Mesh(geometry, material);
-scene.add(cube);
 camera.position.z = 5;
 
+let stickmanMaterial: MeshBasicMaterial = new MeshBasicMaterial({color: 'black'});
+let stickman: Mesh = new Mesh(geometry, stickmanMaterial);
+stickman.position.z = 4;
+stickman.position.x = 0.5;
+scene.add(stickman);
+
+let grid = new GridHelper(10,10);
+grid.rotateX(Math.PI * 0.5);
+scene.add(grid);
 function animate() {
-    requestAnimationFrame( animate );
-    renderer.render( scene, camera );
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
 }
+window.onkeydown =  (e) => {
+    console.log(e.code);
+    switch (e.code) {
+        case 'ArrowDown':
+            stickman.position.y -= 0.1;
+            break;
+        case 'ArrowUp' :
+            stickman.position.y += 0.1;
+            break;
+        case 'ArrowLeft':
+            stickman.position.x -= 0.1;
+            break;
+        case 'ArrowRight':
+            stickman.position.x += 0.1;
+            break;
+        case 'Enter':
+            stickman.position.z -= 0.1;
+            break;
+        case 'Space':
+            stickman.rotateZ(Math.PI * 0.2)
+
+    }
+};
+
+window.document.querySelectorAll("#test")[0].addEventListener('click', () => {
+    stickman.position.z += 0.1;
+});
+
 animate();
